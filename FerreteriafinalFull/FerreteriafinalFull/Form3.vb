1188495b86feb8714d2401cb2bc7a3418d9c2f3e
EFRAIN <EFR714@HOTMAIL.COM>
2017-03-27 18:17:38 -0600

    CAMBIO

--- FerreteriafinalFull/FerreteriafinalFull/Form3.vb
+++ FerreteriafinalFull/FerreteriafinalFull/Form3.vb
@@ -9,6 +9,9 @@ Public Class Form3
 
     Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
         Try
+            If Not Directory.Exists("C:\Programacion1\") Then
+                Directory.CreateDirectory("C:\Programacion1\")
+            End If
             fs = New FileStream("C:\Programacion1\Datos.txt", FileMode.Append, FileAccess.Write)
             sw = New StreamWriter(fs)
             MsgBox("Abriendo archivo")
@@ -21,5 +24,16 @@ Public Class Form3
             MsgBox("Cerrando el archivo")
             If (Not sw Is Nothing) Then sw.Close()
         End Try
+        txtCantidad.Clear()
+        txtCodigoProducto.Clear()
+        txtCodProveedor.Clear()
+        txtNombre.Clear()
+        txtPrecioUnitario.Clear()
+        txtPrecioVenta.Clear()
+        txtProveedor.Clear()
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+
     End Sub
 End Class
