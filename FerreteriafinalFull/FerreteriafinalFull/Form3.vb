cd14fa2a2dc294aba7e390b7a58ae192551a2171
EFRAIN <EFR714@HOTMAIL.COM>
2017-03-30 09:07:13 -0600

    ELIMINAR

--- FerreteriafinalFull/FerreteriafinalFull/Form3.vb
+++ FerreteriafinalFull/FerreteriafinalFull/Form3.vb
@@ -69,26 +69,31 @@ Public Class Form3
     End Sub
 
     Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        'crea el archivo temporal
+        sw = New StreamWriter("c:\Programacion1\temp.txt")
+        'lee los datos
+        sr = New StreamReader("C:\Programacion1\Datos.txt")
 
+        'Aqui se guarda cada linea
+        Dim line As String
+        line = sr.ReadLine()
+        Do While Not line Is Nothing 'Verifica que la linea NO esté vacia
+            Dim categorias() As String = line.Split(" ") 'divide en secciones la cadena
+            If categorias(0) <> cbxProductos.Text Then 'La condicion es que mientras line sea diferente al codigo, se escribe todo, menos donde está el código
+                sw.WriteLine(line)
+            End If
 
+            line = sr.ReadLine()
+        Loop
+        'Se cierra la lectura y escritura
+        sw.Close()
+        sr.Close()
 
-        'Using fileWrite As New StreamWriter("c:\Programacion1\temp.txt")
-        '    Using fielRead As New StreamReader("C:\Programacion1\Datos.txt")
-        '        Dim line As [String]
-
-        '        While (InlineAssignHelper(line, fielRead.ReadLine())) IsNot Nothing
-        '            Dim datos As String() = line.Split(New Char() {","c})
-        '            If datos(0) <> txtBuscar.Text Then
-        '                fileWrite.WriteLine(line)
-
-        '            End If
-        '        End While
-        '    End Using
-        'End Using
-
-        ''aqui se renombrea el archivo temporal
-        'File.Delete("c:\Programacion1\Datos.txt")
-        'File.Move("c:\Programacion1\temp.txt", "c:\Programacion1\Datos.txt")
+        'aqui se renombrea el archivo temporal
+        File.Delete("c:\Programacion1\Datos.txt")
+        File.Move("c:\Programacion1\temp.txt", "c:\Programacion1\Datos.txt")
+        leerProductos() 'Carga los productos en el combobox
+        CargarProductos() 'Carga los productos en el dataGridView
     End Sub
 
     Private Sub dgvProductos_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvProductos.CellContentClick
